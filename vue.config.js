eba2d838c3c4a5d25643ab4cbd4cc054ebf10bb3
Simon Kaminski <szy.kaminski@gmail.com>
2022-09-04 22:06:24 +0200

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'vue-todo-list'
+  publicPath: '/vue-todo-list/'
 })
